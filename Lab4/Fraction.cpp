6801645e78291e15e22f40e65c85e974d08fb65b
Andrew Marshall <amarshall.iv@gmail.com>
2010-02-28 16:12:16 -0500

    Implemented overloaded operators

--- Lab4/Fraction.cpp
+++ Lab4/Fraction.cpp
@@ -11,10 +11,14 @@ using namespace std;
 Fraction::Fraction() {
 }
 
-Fraction::Fraction(int num, int denom) {
+Fraction::Fraction(int cNum, int cDenom) {
+	num = cNum;
+	denom = cDenom;
 }
 
 Fraction::Fraction(const Fraction& f) {
+	num = f.num;
+	denom = f.denom;
 }
 
 Fraction::~Fraction() {
@@ -24,19 +28,41 @@ bool operator==(const Fraction & f1, const Fraction & f2) {
 }
 
 Fraction Fraction::operator+(const Fraction &f1) const {
+	Fraction newFrac;
+	newFrac.num = num*f1.denom + f1.num*denom;
+	newFrac.denom = denom * f1.denom;
+	return newFrac;
 }
 
 Fraction Fraction::operator-(const Fraction &f1) const {
+	Fraction newFrac;
+	newFrac.num = num*f1.denom - f1.num*denom;
+	newFrac.denom = denom * f1.denom;
+	return newFrac;
 }
 
 Fraction Fraction::operator*(const Fraction &f1) const {
+	Fraction newFrac;
+	newFrac.num = num * f1.num;
+	newFrac.denom = denom * f1.denom;
+	return newFrac;
 }
 
 const Fraction& Fraction::operator=(const Fraction &f) {
+	num = f.num;
+	denom = f.denom;
+	return *this;
 }
 
 std::istream& operator>>(std::istream& in, Fraction & f) {
+	cout << "Numerator: ";
+	in >> f.num;
+	cout << "Denominator: ";
+	in >> f.denom;
+	return in;
 }
 
 std::ostream& operator<<(std::ostream& out, const Fraction& f) {
+	out << f.num << '/' << f.denom;
+	return out;
 }
