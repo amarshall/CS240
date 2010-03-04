d792f7e97a9b8eade07befc3f0e466dd24543794
Andrew Marshall <amarshall.iv@gmail.com>
2010-03-04 14:36:16 -0500

    Implemented all methods in LinkedList except display

--- Lab5/LinkedList.cpp
+++ Lab5/LinkedList.cpp
@@ -7,23 +7,38 @@
 #include "LinkedList.h"
 
 LinkedList::LinkedList() {
-
+	first = NULL;
 }
 
 void LinkedList::insertLastNode(StudentRecord* srIn) {
-
+	if(first != NULL) {
+		Node * tmp;
+		for(tmp=first; tmp->next != NULL; tmp=tmp->next);
+		tmp->next = new Node(srIn, NULL);
+	} else {
+		first = new Node(srIn, NULL);
+	}
 }
 
 void LinkedList::removeFirstNode() {
-
+	Node * tmp = first->next;
+	delete first;
+	first = tmp;
 }
 
 void LinkedList::displayList() {
-
+	
 }
 
 LinkedList::~LinkedList() {
-
+	if(first != NULL) {
+		Node * tmp = first;
+		while(tmp != NULL) {
+			Node * next = tmp->next;
+			delete tmp;
+			tmp = next;
+		}
+	}
 }
 
 
