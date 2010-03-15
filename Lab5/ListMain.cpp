dfc9667bfd0c4bf077e46d320f32fc90b282ba1d
Andrew Marshall <amarshall.iv@gmail.com>
2010-03-14 23:30:27 -0400

    Add more test cases

--- Lab5/ListMain.cpp
+++ Lab5/ListMain.cpp
@@ -19,22 +19,22 @@ int main() {
 	courseList[1] = 220;
 	courseList[2] = 240;
   
-	StudentRecord *sr = new StudentRecord(111, 3.11, courseList, 3);
-	myList->insertLastNode(sr);
-
-	StudentRecord *sr2 = new StudentRecord(222, 3.22, courseList, 3);
-	myList->insertLastNode(sr2);
-
-	StudentRecord *sr3 = new StudentRecord(333, 3.333, courseList, 3);
-	myList->insertLastNode(sr3);
-
-	StudentRecord *sr4 = new StudentRecord(444, 3.44, courseList, 3);
-	myList->insertLastNode(sr4);
-
+	myList->insertLastNode(new StudentRecord(111, 3.11, courseList, 3));
+	myList->insertLastNode(new StudentRecord(222, 3.22, courseList, 3));
+	myList->insertLastNode(new StudentRecord(333, 3.333, courseList, 3));
+	myList->insertLastNode(new StudentRecord(444, 3.44, courseList, 3));
+	myList->insertLastNode(new StudentRecord(555, 1.0, NULL, 0));
 	myList->displayList();
-
 	myList->removeFirstNode();
-
+	cout << "-------------------------------------------" << endl;
+	myList->displayList();
+	delete myList;
+	
+	cout << "-------------------------------------------" << endl;
+	myList = new LinkedList();
+	myList->displayList();
+	myList->insertLastNode(new StudentRecord(555, 1.0, NULL, 0));
+	cout << "-------------------------------------------" << endl;
 	myList->displayList();
 	
 	delete myList;
