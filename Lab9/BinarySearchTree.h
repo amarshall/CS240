9c41f53310d317d5bc8fe95c581fbee70ca1a7a0
Andrew Marshall <amarshall.iv@gmail.com>
2010-04-15 20:53:50 -0400

    Add BinarySearchTree & TreeNode

---
+++ Lab9/BinarySearchTree.h
@@ -0,0 +1,22 @@
+/**
+ * CS240 Lab 9
+ * BinarySearchTree.h
+ * @author J. Andrew Marshall (jmarsha6)
+ * @author Ahmet Iscen (aiscen1)
+ */
+
+#include "TreeNode.h"
+
+class BinarySearchTree {
+	private: 
+		TreeNode* root;
+		bool insertHelper(TreeNode *node, int elem);
+		bool searchHelper(TreeNode *node, int elem) const;
+		void traversalHelper(TreeNode *node);
+	
+	public:
+		BinarySearchTree();
+		bool search(int elem) const;
+		bool insert(int elem);
+		void traverseInOrder();
+};
