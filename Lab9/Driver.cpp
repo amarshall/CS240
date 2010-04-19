f12dce7767e538e1f2ca2afce0d9c54ad631aef7
Ahmet Iscen <aiscen1@binghamton.edu>
2010-04-19 13:00:03 -0400

    Finished the searchClass.cpp

--- Lab9/Driver.cpp
+++ Lab9/Driver.cpp
@@ -12,15 +12,6 @@
 using namespace std;
 
 int main(){
-/** ----------FOR TESTING--------------
-	SearchClass search1;
-	int* arr1 = search1.createInputArray(9,time(NULL));
-	for( int i=0; i<9;i++) cout<<arr1[i]<<endl;
-	
-	//Change the value to be searched for different cases.
-	cout<< "linear: "<< search1.linearSearch(10) << endl;
-	cout<< "binary: "<< search1.itBinarySearch(10) << endl;
-	cout<< "binary-recursion: "<< search1.recBinarySearch(10) << endl;
-**/
+
 	
 }
